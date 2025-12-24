13a23cf4768395953991fac139ed686726ec7341
Lilian Reisinho <lilianreisinho@gmail.com>
2025-12-24 15:56:41 +0100

    feature : début de l'ui avec le routage des pages et la homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
-import HealthPage from '@/features/health/pages/HealthPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './ui/components/navbar/Navbar';
+import { HomePage } from './ui/pages/homepage/HomePage';
+import { QuizSession } from './ui/pages/quizzSession/QuizzSession';
+import { CreateCardPage } from './ui/pages/cards/CreateCardPage';
+import { CardsListPage } from './ui/pages/cards/CardsListPage';
 
-export default function App() {
-  return <HealthPage />;
-}
+
+export const App = () => {
+  return (
+    <Router>
+      <div className="app-container">
+        {/* Navbar reste visible sur toutes les pages */}
+        <Navbar />
+
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/quizz" element={<QuizSession />} />
+            <Route path="/create-card" element={<CreateCardPage />} />
+            <Route path="/cards" element={<CardsListPage />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+};

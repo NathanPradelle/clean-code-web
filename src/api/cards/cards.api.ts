61812ded2d424367792673adbc62fb63b3ce3eec
Lilian Reisinho <lilianreisinho@gmail.com>
2025-12-24 14:56:58 +0100

    fix : ajustement de la requete de creation de card qui ne pointait jamais vers l'api

--- src/api/cards/cards.api.ts
+++ src/api/cards/cards.api.ts
@@ -1,4 +1,4 @@
-import { getJson } from "../httpClient";
+import { getJson, postJson } from "../httpClient";
 import { CardApi, CreateCardApi } from "../cards/cards.type";
 import { mapCardApiToDomain } from "../cards/cards.mapper";
 import { Card } from "../../domain/card";
@@ -28,18 +28,6 @@ export async function fetchCards(tag?: string): Promise<Card[]> {
  * @returns La carte créée.
  */
 export async function createCard(cardToCreate: CreateCardApi): Promise<Card> {
-    const res = await fetch('/cards', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(cardToCreate),
-    });
-
-    if (!res.ok) {
-        throw new Error(`HTTP ${res.status}`);
-    }
-
-    const cardApi = (await res.json()) as CardApi;
+    const cardApi = await postJson<CardApi>('/cards', cardToCreate);
     return mapCardApiToDomain(cardApi);
 }
--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -9,3 +9,20 @@ export async function getJson<T>(path: string): Promise<T> {
 
   return (await res.json()) as T;
 }
+
+export async function postJson<T>(path: string, body: any): Promise<T> {
+  const res = await fetch(`${baseUrl}${path}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+
+  if (!res.ok) {
+    throw new Error(`HTTP ${res.status}`);
+  }
+
+  return (await res.json()) as T;
+}
+
